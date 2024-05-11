cf20be2c23c12a64ca58503f55e63f6316d9af9b
lcoronat <lcoronat@unal.edu.co>
2024-05-11 12:33:11 -0500

    mod 3

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const bodyE1 = document.querySelector("body")
 bodyE1.addEventListener("mousemove", (event) => {
     const xPos = event.offsetX
     const yPos = event.offsetY
-})
+    const spanE1 = document.createElement("span");
+    bodyE1.appendChild(spanE1);
+    spanE1.style.left = xPos + "px";
+    spanE1.style.left = yPos + "py";
+    spanE1.appendChild(spanE1);
+});
