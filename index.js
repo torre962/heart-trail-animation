1fc659cdacb13ff8619163592775fd13429162b7
lcoronat <lcoronat@unal.edu.co>
2024-05-27 17:17:56 -0500

    modificacion 5 y ajuste de js

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ bodyEl.addEventListener("mousemove", (event) => {
     const spanEl = document.createElement("span");
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width= size + "px";
+    spanEl.style.height= size + "px";
     bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
 });
